1bb381b452830f008a0f028558afa75e199fe0d1
박호철 <Hocheol@baghocheol-ui-MacBook-Pro.local>
2017-05-10 17:06:10 +0900

    bluetooth test

--- app/src/main/java/univ/ajou/godoriapp/MainActivity.java
+++ app/src/main/java/univ/ajou/godoriapp/MainActivity.java
@@ -1,31 +1,46 @@
 package univ.ajou.godoriapp;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int REQUEST_ENABLE_BT = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Godori App");
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Put your hands up!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        // Bluetooth
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        }
     }
 
     @Override
@@ -48,5 +63,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.e("SDF", "SDF : "+requestCode+ " & resultCode : " +resultCode);
+        if(resultCode == REQUEST_ENABLE_BT) {
+            Toast.makeText(this, "굿 보이", Toast.LENGTH_SHORT).show();
+        }
+        else if(resultCode == RESULT_CANCELED) {
+            Toast.makeText(this, "블루투스 켜라 임마", Toast.LENGTH_SHORT).show();
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
